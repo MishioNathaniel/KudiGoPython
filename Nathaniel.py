254799bbfb1ae9f9a796269dfa898446ab5418ca
Nathaniel <nathanielmishio@yahoo.com>
2020-04-14 20:19:46 +0000

    First python assignment

---
+++ Nathaniel.py
@@ -0,0 +1,17 @@
+
+ 
+def addTwoValues(numberOne, numberTwo):
+    return numberOne + numberTwo
+print(addTwoValues(1,2))
+    
+def subtractTwoValues(numberOne, numberTwo):
+    return numberOne - numberTwo
+print(subtractTwoValues(1,2))
+    
+def multiplyTwoValues(numberOne, numberTwo):
+    return numberOne * numberTwo
+print(multiplyTwoValues(1,2))
+    
+def divideTwoValues(numberOne, numberTwo):
+    return numberOne / numberTwo
+print(divideTwoValues(1,2))
