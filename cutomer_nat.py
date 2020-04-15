8480940d51b173e0e3836b97a9704a5b7851c84b
Nathaniel <nathanielmishio@yahoo.com>
2020-04-15 23:34:01 +0000

    second python assignment

--- cutomer_nat.py
+++ cutomer_nat.py
@@ -1,4 +1,4 @@
-
+#creating class for customer
 class Customer:
   def __init__(mysillyobject,Id,first_name,last_name, phonenumber,email):
     mysillyobject.Id = Id
@@ -7,7 +7,7 @@ class Customer:
     mysillyobject.phonenumber = phonenumber
     mysillyobject.email = email
 
-
+#function for customer details 
   def showCustomer(abc):
     print("\n")
     print("---Customer Details---")
@@ -17,12 +17,15 @@ class Customer:
     print("Phone number: ", abc.phonenumber)
     print("email: ", abc.email)
 
+#creating class for order
 class order:
     def __init__(mysillyobject,order_Id,item,price,total):
         mysillyobject.order_Id =order_Id
         mysillyobject.item = item
         mysillyobject.price= price
         mysillyobject.total = total
+
+ #function for order details        
     def showOrder(wendy):
         print("\n")
         print("--- ORDERS----")
@@ -30,25 +33,29 @@ class order:
         print("item: " + wendy.item)
         print("price: " , wendy.price)
         print("total: ", wendy.total)
-    
+
+#creating class for merchant details    
 class merchant:
     def __init__(mysillyobject,merchant_Id,merchant_name):
         mysillyobject.merchant_Id =merchant_Id
         mysillyobject.merchant_name = merchant_name
         
-
+#function for merchant details 
     def showMerchant(nathaniel):
         print("\n")
         print("--- Merchant Details----")
         print("Merchant id: ",  nathaniel.merchant_Id)
         print("Merchant name: " + nathaniel.merchant_name)
 
+#creating class for order
 class order:
     def __init__(mysillyobject,order_Id,item,price,total):
         mysillyobject.order_Id =order_Id
         mysillyobject.item = item
         mysillyobject.price= price
         mysillyobject.total = total
+
+  #function for order details      
     def showOrder(wendy):
         print("\n")
         print("--- ORDERS----")
@@ -56,14 +63,15 @@ class order:
         print("item: " + wendy.item)
         print("price: " , wendy.price)
         print("total: ", wendy.total)
-    
+
+#creating class for product    
 class product:
     def __init__(mysillyobject,product_Id,product_name,expiry_date):
         mysillyobject.product_Id = product_Id
         mysillyobject.product_name = product_name
         mysillyobject.expiry_date = expiry_date
         
-
+#function for order details   
     def showProduct(wenat):
         print("\n")
         print("--- Product Details----")
@@ -71,13 +79,14 @@ class product:
         print("Product name: " + wenat.product_name)
         print("Product expiry: "+ wenat.expiry_date)
 
+#creating class for user
 class user:
     def __init__(mysillyobject,user_Id,user_name,title):
         mysillyobject.user_Id = user_Id
         mysillyobject.user_name = user_name
         mysillyobject.title = title
         
-
+#function for user details   
     def showUser(forever):
         print("\n")
         print("--- User Details----")
@@ -85,14 +94,14 @@ class user:
         print("User name: " + forever.user_name)
         print("User's Title : "+ forever.title)       
       
-      
+  #creating class for transaction    
 class transaction:
     def __init__(mysillyobject,transaction_Id,transaction_date,transaction_time):
         mysillyobject.transaction_Id = transaction_Id
         mysillyobject.transaction_date = transaction_date
         mysillyobject.transaction_time = transaction_time
         
-
+#function for transaction details
     def showTransaction(Omega):
         print("\n")
         print("--- Transaction Details----")
@@ -101,6 +110,7 @@ class transaction:
         print("Time: ", Omega.transaction_time)   
 
 
+#calling all functions and passing arguments to parameters
 p1 = Customer(934 , "John","mintah", 36, "nathanielmishio@yahoo.com")
 p1.showCustomer()
 
